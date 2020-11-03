22c5e3c44050b2c0cc04f6f58d850d0d3b92f0fe
moacyr074 <moacyr074@yahoo.com>
2020-11-02 22:24:15 -0500

    Initial Git

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) => {
+    console.log('Server received request 🔥🔥🔥');
+    res.end('Hello World');
+}
+
+const server = http.createServer(handler);
+
+server.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is listening on port  ${port}`)
+    }
+})
